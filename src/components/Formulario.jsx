8a202a50ab25336115550e5d9204685b57555aa4
Juan8516 <juan.ind85@gmail.com>
2023-09-14 20:30:48 -0500

    se agrega formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,28 @@
-function Formulario() {
+import { useState, useEffect } from "react";
+
+const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [alta, setAlta] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const [error, setError] = useState(false);
+
+
+  const handleSubmit = (e) => {
+   e.preventDefault();
+    console.log('Enviando...')
+
+    //Validacion Formulario
+    if ([nombre, propietario, email, alta, sintomas].includes('')) {
+      console.log('Ingrese toda la informacion')
+
+      setError(true)
+      return;
+    } 
+      setError(false)
+  }
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -10,58 +34,87 @@ function Formulario() {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit = {handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5">
+
+        { error && 
+        <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+          <p>Todos los campos son obligatorios</p>
+        </div> 
+        } 
+
         <div className="mb-5">
-          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+              Nombre Mascota
+            </label>
 
           <input
               id="mascota" 
               type="text"
               placeholder="Nombre de la mascota"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value = {nombre}
+              onChange = {(e) => setNombre(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+              Nombre Propietario
+            </label>
 
           <input
               id="propietario" 
               type="text"
               placeholder="Nombre Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value = {propietario}
+              onChange = {(e) => setPropietario(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+              Email
+            </label>
 
           <input
               id="email" 
               type="email"
               placeholder="Email Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value = {email}
+              onChange = {(e) => setEmail(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Alta</label>
+          <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+              Alta
+            </label>
 
           <input
               id="alta" 
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value = {alta}
+              onChange = {(e) => setAlta(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
-          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Sintomas</label>
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+              Sintomas
+            </label>
 
           <textarea
               id="sintomas" 
               type="date"
               placeholder="Descripcion Sintomas"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value = {sintomas}
+              onChange = {(e) => setSintomas(e.target.value)}
           />
         </div>
 
