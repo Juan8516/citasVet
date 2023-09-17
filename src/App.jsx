b85ff54f3ea2f864ec8ad3544dfaf2c365a0d894
Juan8516 <juan.ind85@gmail.com>
2023-09-17 16:55:29 -0500

    se agrega funcionalidad de eliminar paciente

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
 
+  const eliminarPaciente = id => {
+      const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+      setPacientes(pacientesActualizados)
+  }
+
   return (
     <div className="container mx-auto">
       <Header/>
@@ -24,6 +29,7 @@ const App = () => {
         <ListadoPacientes
           pacientes = {pacientes}
           setPaciente = {setPaciente}
+          eliminarPaciente = {eliminarPaciente}
         />
       </div>
 
