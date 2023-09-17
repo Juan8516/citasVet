bd05f05a2341ec9b1023c5e50b3ae421024fd4c4
Juan8516 <juan.ind85@gmail.com>
2023-09-17 16:25:25 -0500

    se agrega funcionalidad de edicion de los registros

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Error from "./Error";
 
-const Formulario = ( {pacientes, setPacientes, paciente }) => {
+const Formulario = ( {pacientes, setPacientes, paciente }, setPaciente) => {
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
   const [email, setEmail] = useState('');
@@ -49,11 +49,22 @@ const Formulario = ( {pacientes, setPacientes, paciente }) => {
         propietario, 
         email, 
         alta, 
-        sintomas,
-        id: generarId()
+        sintomas
       }
 
-      setPacientes([...pacientes, objetoPaciente])
+      if (paciente.id) {
+        //Editando el registro
+        objetoPaciente.id = paciente.id
+        const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+
+        setPacientes(pacientesActualizados)
+        setPaciente({})
+
+      }else {
+        //Nuevo registro
+        objetoPaciente.id = generarId()
+        setPacientes([...pacientes, objetoPaciente])
+      }
 
       //Reinicio Form
       setNombre('')
